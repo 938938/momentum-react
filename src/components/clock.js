6408b3ba48f3469f58b87d183816177d64c82e3e
938938 <938kjh@gmail.com>
2022-05-23 04:31:46 +0900

    시간 기능 수정

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,23 +1,23 @@
+import { cleanup } from '@testing-library/react';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko'
 import { useEffect, useRef, useState } from 'react';
 
 const Clock = () => {
 
-  let nowClock = dayjs().format('YYYY년 MM월 DD일 / HH:mm:ss');
-  const [time, setTime] = useState(nowClock);
-  const interval = useRef(null);
+  const [time, setTime] = useState(dayjs());
+  const clock = useRef(null);
 
   useEffect(()=>{
-    interval.current = setInterval(()=>{
-      setTime(nowClock);
-    },1000)
-    return () => clearInterval(interval.current);
+    clock.current = setInterval(()=>{
+      setTime(dayjs())
+    }, 1000)
+    return ()=>{cleanup(clock.current)}
   })
 
   return(
     <div>
-      {time}
+      {time.format('YYYY년 MM월 DD일 HH:mm:ss')}
     </div>
   )
 }
