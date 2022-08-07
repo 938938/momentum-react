5f237b690ce62c57cbbb3886511b9b369a7a2611
938938 <938kjh@gmail.com>
2022-08-07 23:53:08 +0900

    commit

--- src/components/MemoButton.js
+++ src/components/MemoButton.js
@@ -1,6 +1,8 @@
+import styled from 'styled-components';
+
 const MemoButton = ({ text1, text2, type1, type2, onClick1, onClick2 }) => {
   return (
-    <div className="MemoButton">
+    <div style={styles.MemoButton}>
       <button className={['MemoButton_left', `MemoButton_${type1}`].join(' ')} onClick={onClick1}>
         {text1}
       </button>
@@ -14,5 +16,30 @@ const MemoButton = ({ text1, text2, type1, type2, onClick1, onClick2 }) => {
 MemoButton.defaultProps = {
   type: 'default',
 };
+
+const styles = StyleSheet.create({
+  MemoButton:{
+    height: 30,
+    marginTop: 20,
+    justifycontent: center,
+    margin: 0,
+  },
+  MemoButton_left:{
+    border: 0,
+    borderRadius: 10,
+    width: 50,
+    height: 100,
+    cursor: pointer,
+    color: white,
+  },
+  MemoButton_right:{
+    border: 0,
+    borderRadius: 10,
+    width: 50,
+    height: 100,
+    cursor: pointer,
+    color: white,
+  }
+})
 
 export default MemoButton;
