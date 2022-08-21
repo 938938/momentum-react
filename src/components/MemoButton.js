6b412d0ec9bcf33264761c16f55098e1a8073426
938938 <938kjh@gmail.com>
2022-08-21 23:52:43 +0900

    CSS 수정

--- src/components/MemoButton.js
+++ src/components/MemoButton.js
@@ -1,5 +1,3 @@
-import styled from 'styled-components';
-
 const MemoButton = ({ text1, text2, type1, type2, onClick1, onClick2 }) => {
   return (
     <div style={styles.MemoButton}>
@@ -16,30 +14,3 @@ const MemoButton = ({ text1, text2, type1, type2, onClick1, onClick2 }) => {
 MemoButton.defaultProps = {
   type: 'default',
 };
-
-const styles = StyleSheet.create({
-  MemoButton:{
-    height: 30,
-    marginTop: 20,
-    justifycontent: center,
-    margin: 0,
-  },
-  MemoButton_left:{
-    border: 0,
-    borderRadius: 10,
-    width: 50,
-    height: 100,
-    cursor: pointer,
-    color: white,
-  },
-  MemoButton_right:{
-    border: 0,
-    borderRadius: 10,
-    width: 50,
-    height: 100,
-    cursor: pointer,
-    color: white,
-  }
-})
-
-export default MemoButton;
