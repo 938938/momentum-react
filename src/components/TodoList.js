959837f9af00bfd8acc73175283f31e5889930b0
938938 <938kjh@gmail.com>
2022-09-10 22:54:39 +0900

    ::styled-components 적용중

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { TodoStateContext } from '../pages/Main';
 // Main.js에서 TodoStateContext를 불러와야한다
 import TodoItem from './TodoItem';
@@ -7,7 +8,7 @@ const TodoList = () => {
   const todolist = useContext(TodoStateContext);
   // useContext를 사용, data를 전달받는다
   return (
-    <div className="TodoList">
+    <TodoListBox>
       <div>
         {todolist.map(
           (it) => (
@@ -16,7 +17,7 @@ const TodoList = () => {
           // TodoItem에 따로 onRemove 등의 함수를 전달하지 않아도 됨.
         )}
       </div>
-    </div>
+    </TodoListBox>
   );
 };
 
@@ -25,3 +26,14 @@ TodoList.defaultProps = {
 };
 
 export default React.memo(TodoList);
+
+const TodoListBox = styled.div`
+  height: 59vh;
+  margin: 0;
+  overflow: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`
